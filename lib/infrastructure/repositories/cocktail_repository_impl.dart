bd8aaefd1ab65e74ea58bf4c04a7080ec37d7754
germandbl <gdb.lopez@outlook.com>
2024-05-26 16:55:02 -0600

    CocktailRepository implementacion

---
+++ lib/infrastructure/repositories/cocktail_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'package:cocktaildb_app/domain/entities/drink.dart';
+import 'package:cocktaildb_app/domain/repositories/drinks_repository.dart';
+import 'package:cocktaildb_app/infrastructure/datasources/cocktaildb_datasource.dart';
+
+class CocktailRepositoryImpl extends DrinksRepository {
+
+  final CocktailDBDatasource datasource;
+  CocktailRepositoryImpl(this.datasource);
+
+  @override
+  Future<Drink> getDrinkById(int id) {
+    // TODO: implement getDrinkById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Drink>> getDrinksByFirstLetter(String letter) {
+    // TODO: implement getDrinksByFirstLetter
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Drink>> getDrinksByName(String name) {
+    return datasource.getDrinksByName(name);
+  }
+
+}
