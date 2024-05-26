d9e9be6f9a44293deb148484391ce9148f9a7dbc
germandbl <gdb.lopez@outlook.com>
2024-05-26 16:29:26 -0600

    Implementacion base de la vista de busqueda

--- lib/presentation/views/search_view.dart
+++ lib/presentation/views/search_view.dart
@@ -5,6 +5,43 @@ class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: TextFormField(
+              onFieldSubmitted: (value) => print('VALOR ENVIADO: $value'),
+              textAlignVertical: TextAlignVertical.center,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                labelText: 'Buscar un cocktail',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10)
+                  )
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          Expanded(
+            child: ListView(
+              children: [
+                  ListTile(
+                    onTap: (){},
+                    title: const Text('Whisky'),
+                    subtitle: const Text('Una bebida hecha a base de vino'),
+                    leading: Image.network('https://www.singlemalt.ph/cdn/shop/products/Untitleddesign-2021-03-24T165234.115_1024x.png?v=1616575964', height: 120, width: 50,),
+                    trailing: const Icon(Icons.arrow_forward_ios_rounded),
+                  )                
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
