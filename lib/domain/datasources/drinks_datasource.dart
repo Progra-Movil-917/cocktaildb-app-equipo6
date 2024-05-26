5e2acb97e129f98bdaa88b96b523745e75cf01e2
germandbl <gdb.lopez@outlook.com>
2024-05-26 16:24:33 -0600

    DrinksDatasource y DrinksRepository implementados en el dominio

---
+++ lib/domain/datasources/drinks_datasource.dart
@@ -0,0 +1,7 @@
+import 'package:cocktaildb_app/domain/entities/drink.dart';
+
+abstract class DrinksDatasource {
+  Future<Drink> getDrinkById( int id );
+  Future<List<Drink>> getDrinksByName( String name );
+  Future<List<Drink>> getDrinksByFirstLetter( String letter );
+}
