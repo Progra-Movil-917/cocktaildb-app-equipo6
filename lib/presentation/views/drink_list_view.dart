359f238ec314fcae3f4b62f4a234d132bd4ec59d
germandbl <gdb.lopez@outlook.com>
2024-05-26 10:45:14 -0600

    Vistas base de la app implementadas

---
+++ lib/presentation/views/drink_list_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrinkListView extends StatelessWidget {
+  const DrinkListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
