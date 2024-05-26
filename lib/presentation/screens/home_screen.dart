9b5cb8648f26a5593b99c807b65f0487b4ef635c
germandbl <gdb.lopez@outlook.com>
2024-05-26 10:25:37 -0600

    Layout base de la HomeScreen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -5,6 +5,27 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text("Cocktails App"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Inicio',
+            icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+            label: 'Lista de bebidas',
+            icon: Icon(Icons.view_list)
+          ),
+          BottomNavigationBarItem(
+            label: 'Buscar',
+            icon: Icon(Icons.search_rounded)
+          ),
+        ]
+      ),
+    );
   }
 }
