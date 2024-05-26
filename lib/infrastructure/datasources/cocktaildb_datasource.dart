bb9e564c3bfc8bb0c1166750657aaf39c4e101bf
germandbl <gdb.lopez@outlook.com>
2024-05-26 16:51:51 -0600

    CocktailDB Models implementados y Datasource, se añade paquete dio para peticiones http

---
+++ lib/infrastructure/datasources/cocktaildb_datasource.dart
@@ -0,0 +1,40 @@
+import 'package:cocktaildb_app/domain/datasources/drinks_datasource.dart';
+import 'package:cocktaildb_app/domain/entities/drink.dart';
+import 'package:cocktaildb_app/infrastructure/mappers/drink_mapper.dart';
+import 'package:cocktaildb_app/infrastructure/models/cocktaildb_response.dart';
+import 'package:dio/dio.dart';
+
+class CocktailDBDatasource extends DrinksDatasource {
+  final dio = Dio(BaseOptions(
+    baseUrl: 'www.thecocktaildb.com/api/json/v1/1',
+  ));
+
+  List<Drink> _jsonToDrinks(Map<String, dynamic> json) {
+    final cocktaildDBResponse = CocktailDBResponse.fromJson(json);
+
+    final List<Drink> drinks = cocktaildDBResponse.cocktails
+        .map((drink) => DrinkMapper.cocktailDBToEntity(drink))
+        .toList();
+
+    return drinks;
+  }
+
+  @override
+  Future<Drink> getDrinkById(int id) {
+    // TODO: implement getDrinkById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Drink>> getDrinksByFirstLetter(String letter) {
+    // TODO: implement getDrinksByFirstLetter
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Drink>> getDrinksByName(String name) async {
+    final response = await dio.get('/search.php?s=$name');
+
+    return _jsonToDrinks(response as Map<String, dynamic>);
+  }
+}
--- lib/infrastructure/models/cocktaildb_response.dart
+++ lib/infrastructure/models/cocktaildb_response.dart
@@ -1,15 +1,17 @@
-class CocktailsResponse {
-    final List<Map<String, String?>> drinks;
+import 'package:cocktaildb_app/infrastructure/models/cocktaildb.dart';
 
-    CocktailsResponse({
-        required this.drinks,
-    });
+class CocktailDBResponse {
+  final List<CocktailDB> cocktails;
 
-    factory CocktailsResponse.fromJson(Map<String, dynamic> json) => CocktailsResponse(
-        drinks: List<Map<String, String?>>.from(json["drinks"].map((x) => Map.from(x).map((k, v) => MapEntry<String, String?>(k, v)))),
-    );
+  CocktailDBResponse({
+    required this.cocktails,
+  });
 
-    Map<String, dynamic> toJson() => {
-        "drinks": List<dynamic>.from(drinks.map((x) => Map.from(x).map((k, v) => MapEntry<String, dynamic>(k, v)))),
-    };
+  factory CocktailDBResponse.fromJson(Map<String, dynamic> json) =>
+      CocktailDBResponse(
+          cocktails: List<CocktailDB>.from(
+              json["drinks"].map((e) => CocktailDB.fromJson(e))));
+
+  Map<String, dynamic> toJson() =>
+      {"drinks": List<dynamic>.from(cocktails.map((x) => x.toJson()))};
 }
