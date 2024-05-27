1634c0db171bac187668233163b8b51bea10e950
germandbl <gdb.lopez@outlook.com>
2024-05-26 22:24:23 -0600

    refactorizacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cocktail_app/models/cocktail_model.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,60 +16,5 @@ class MainApp extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-class CocktailApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lista de Bebidas',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: DrinkList(),
-    );
-  }
-}
-
-class DrinkList extends StatefulWidget {
-  @override
-  _DrinkListState createState() => _DrinkListState();
-}
-
-class _DrinkListState extends State<DrinkList> {
-  final ApiService _apiService = ApiService();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bebidas Aleatorias'),
-      ),
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: _fetchRandomDrink(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            var drink = snapshot.data['drinks'][0];
-            return ListView(
-              children: [
-                ListTile(
-                  leading: Image.network(drink['strDrinkThumb']),
-                  title: Text(drink['strDrink']),
-                ),
-                // Aquí puedes agregar más detalles de la bebida
-              ],
-            );
-          }
-        },
-      ),
-    );
-  }
-
-  Future<Map<String, dynamic>> _fetchRandomDrink() async {
-    return await _apiService.fetchRandomDrink();
   }
 }
